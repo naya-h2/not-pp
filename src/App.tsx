3907f750775f88f19b57cf42e3d993dd666af398
naya-h2 <hw7666@naver.com>
2025-03-05 01:59:22 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import LoginPage from './page/login/page';
 import MainPage from './page/main/page';
 import SignupPage from './page/signup/page';
 
 function App() {
   return (
-    <>
-      <MainPage />
-      {/* <LoginPage /> */}
-      {/* <SignupPage /> */}
-    </>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
   );
 }
 
