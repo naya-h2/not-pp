fd523e5fccfc48e300395005f5912d63e3ddf723
naya-h2 <hw7666@naver.com>
2025-04-04 00:38:24 +0900

    feat: 선배 리스트 무한스크롤 추가

--- src/page/list/page.tsx
+++ src/page/list/page.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import BackButton from '../../components/BackButton';
 import ProfileButton from '../../components/ProfileButton';
 import { MEMBER } from '../../const/member';
@@ -6,20 +6,40 @@ import { useGetProfile } from '../../hooks/useGetProfile';
 import { authInstance } from '../../axios/instance';
 import { MemberType } from '../../type/member';
 import Loading from '../../components/Loading';
+import { useInView } from 'react-intersection-observer';
+import { useEffect, useState } from 'react';
 
 function ListPage() {
   const profile = useGetProfile();
-  const {
-    data: profileList,
-    isLoading,
-    isSuccess,
-  } = useQuery({
-    queryKey: ['profile_list'],
-    queryFn: async () => {
-      const { data } = await authInstance.get('/student/');
-      return data;
-    },
-  });
+  const [ref, inView] = useInView();
+  const [students, setStudents] = useState<MemberType[]>([]);
+
+  const getStudentList = async ({ pageParam }: any) => {
+    const { data } = await authInstance.get(`/student/?page=${pageParam}`);
+    setStudents((prev) => [...prev, ...data.results]);
+    return data;
+  };
+
+  const { fetchNextPage, hasNextPage, isLoading, isSuccess } = useInfiniteQuery(
+    {
+      queryKey: ['profile_list'],
+      queryFn: getStudentList,
+      getNextPageParam: (lastPage) => {
+        if (!lastPage.next) return undefined;
+
+        const url = new URL(lastPage.next);
+        const page = url.searchParams.get('page');
+        return page;
+      },
+      initialPageParam: '1',
+    }
+  );
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   return (
     <>
@@ -41,15 +61,25 @@ function ListPage() {
       <p className="text-grey-500">
         프로필을 클릭하면 상세 프로필을 볼 수 있어요
       </p>
+
       <div className="flex flex-col gap-4 pt-11">
         {isSuccess &&
-          profileList.map((data: MemberType) => (
-            <ProfileButton
-              key={data.student_id}
-              name={data.name}
-              studentId={data.student_id}
-              gender={MEMBER.gender[data.gender]}
-            />
+          (students.length > 0 ? (
+            <>
+              <>
+                {students.map((data: MemberType) => (
+                  <ProfileButton
+                    key={data.student_id}
+                    name={data.name}
+                    studentId={data.student_id}
+                    gender={MEMBER.gender[data.gender]}
+                  />
+                ))}
+              </>
+              <div ref={ref}></div>
+            </>
+          ) : (
+            '아직 등록된 선배가 없어요.'
           ))}
         {isLoading && <Loading />}
       </div>
